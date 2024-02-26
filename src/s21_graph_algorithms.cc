8be84758ef11ae48cbf950e6b099398e9bd3e90e
elidacon <elidacon@student.21-school.ru>
2024-02-26 20:14:41 +0500

    Add DepthFirstSearch(), BreadthFirstSearch() functions

---
+++ src/s21_graph_algorithms.cc
@@ -0,0 +1,65 @@
+#include "s21_graph_algorithms.h"
+
+#include "s21_graph.h"
+
+std::vector<uint32_t> s21::GraphAlgorithms::DepthFirstSearch(
+    s21::Graph &graph, uint32_t start_vertex) {
+  auto size = graph.GetSize();
+  if (start_vertex > size) {
+    throw std::out_of_range("Vertex doesn't exist.");
+  }
+  std::vector<bool> visited(size, false);
+  s21::stack<uint32_t> stack;
+  std::vector<uint32_t> traversal;
+
+  stack.push(start_vertex - 1);
+  visited[start_vertex - 1] = true;
+
+  while (!stack.empty()) {
+    uint32_t vertex = stack.top();
+    stack.pop();
+
+    traversal.push_back(vertex + 1);
+
+    auto matrix = graph.GetGraph();
+    for (uint32_t i = size - 1; i != UINT32_MAX; --i) {
+      if (matrix[vertex * size + i] > 0 && !visited[i]) {
+        stack.push(i);
+        visited[i] = true;
+      }
+    }
+  }
+
+  return traversal;
+}
+
+std::vector<uint32_t> s21::GraphAlgorithms::BreadthFirstSearch(
+    s21::Graph &graph, uint32_t start_vertex) {
+  auto size = graph.GetSize();
+  if (start_vertex > size) {
+    throw std::out_of_range("Vertex doesn't exist.");
+  }
+  std::vector<bool> visited(size, false);
+  s21::queue<uint32_t> queue;
+  std::vector<uint32_t> traversal;
+
+  queue.push(start_vertex - 1);
+  visited[start_vertex - 1] = true;
+
+  while (!queue.empty()) {
+    uint32_t vertex = queue.front();
+    queue.pop();
+
+    traversal.push_back(vertex + 1);
+
+    auto matrix = graph.GetGraph();
+    for (uint32_t i = 0; i < size; ++i) {
+      if (matrix[vertex * size + i] > 0 && !visited[i]) {
+        queue.push(i);
+        visited[i] = true;
+      }
+    }
+  }
+
+  return traversal;
+}
