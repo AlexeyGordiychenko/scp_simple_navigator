9104f1d575fb5c2ade665aa3bfe034e337b54163
elidacon <elidacon@student.21-school.ru>
2024-03-04 23:40:43 +0500

    Add ConsoleMenu class

---
+++ src/s21_console_menu.cc
@@ -0,0 +1,37 @@
+#include "s21_console_menu.h"
+
+#include <limits>
+#include <sstream>
+
+s21::ConsoleMenu::ConsoleMenu(
+    const std::string& message, const std::string& invalid_choice_message,
+    const std::string& prompt,
+    const std::vector<std::pair<std::string_view, std::function<void()>>>&
+        commands,
+    std::istream& in, std::ostream& out)
+    : invalid_choice_message_{invalid_choice_message},
+      commands_{commands},
+      in{in},
+      out{out} {
+  // Construct the prompt message
+  std::stringstream ss;
+  ss << message;
+  for (size_t i = 0; i < commands_.size(); ++i) {
+    ss << '[' << (i + 1) << "] " << commands_[i].first << '\n';
+  }
+  ss << prompt;
+  prompt_ = ss.str();
+}
+
+void s21::ConsoleMenu::operator()() const {
+  uint32_t userChoice;
+  out << '\n';
+  while (!(out << prompt_) || !(in >> userChoice) || userChoice <= 0 ||
+         userChoice > commands_.size() || in.peek() != '\n') {
+    in.clear();  // Clear the error state
+    in.ignore(std::numeric_limits<std::streamsize>::max(),
+              '\n');  // Ignore the rest of the line
+    out << '\n' << invalid_choice_message_;
+  }
+  commands_[userChoice - 1].second();  // Execute the selected function
+}
---
+++ src/s21_console_menu.h
@@ -0,0 +1,34 @@
+#ifndef S21_A2_MENU_H_
+#define S21_A2_MENU_H_
+#include <functional>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace s21 {
+
+class ConsoleMenu {
+ public:
+  ConsoleMenu(
+      const std::string& message, const std::string& invalid_choice_message,
+      const std::string& prompt,
+      const std::vector<std::pair<std::string_view, std::function<void()>>>&
+          commands,
+      std::istream& in = std::cin, std::ostream& out = std::cout);
+  ConsoleMenu(const ConsoleMenu&) = delete;
+  ConsoleMenu& operator=(const ConsoleMenu&) = delete;
+  ConsoleMenu(ConsoleMenu&&) = delete;
+  ConsoleMenu& operator=(ConsoleMenu&&) = delete;
+  ~ConsoleMenu() = default;
+
+  void operator()() const;
+
+ private:
+  std::string invalid_choice_message_;
+  std::string prompt_;
+  std::vector<std::pair<std::string_view, std::function<void()>>> commands_;
+  std::istream& in;
+  std::ostream& out;
+};
+}  // namespace s21
+#endif  // S21_A2_MENU_H_
