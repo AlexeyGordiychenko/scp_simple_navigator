ee205e73ac540489d70523a053155d7589c6adfe
elidacon <elidacon@student.21-school.ru>
2024-03-04 23:40:05 +0500

    Make GraphAlgorithms methods static, remove constructors

--- src/s21_graph_algorithms.h
+++ src/s21_graph_algorithms.h
@@ -16,21 +16,17 @@ struct TsmResult {
 
 class GraphAlgorithms {
  public:
-  // Constructors and the Destructor
-  GraphAlgorithms() = default;
-  GraphAlgorithms(const GraphAlgorithms&) = delete;
-  GraphAlgorithms& operator=(const GraphAlgorithms&) = delete;
-  GraphAlgorithms(GraphAlgorithms&&) = delete;
-  GraphAlgorithms& operator=(GraphAlgorithms&&) = delete;
-  ~GraphAlgorithms() = default;
-
   // Methods
-  std::vector<uint32_t> DepthFirstSearch(Graph& graph, uint32_t start_vertex);
-  std::vector<uint32_t> BreadthFirstSearch(Graph& graph, uint32_t start_vertex);
-  int GetShortestPathBetweenVertices(Graph& graph, int vertex1, int vertex2);
-  void GetShortestPathsBetweenAllVertices(Graph& graph);  // TODO: return value
-  std::vector<uint32_t> GetLeastSpanningTree(Graph& graph);
-  TsmResult& SolveTravelingSalesmanProblem(Graph& graph);
+  static std::vector<uint32_t> DepthFirstSearch(Graph& graph,
+                                                uint32_t start_vertex);
+  static std::vector<uint32_t> BreadthFirstSearch(Graph& graph,
+                                                  uint32_t start_vertex);
+  static int GetShortestPathBetweenVertices(Graph& graph, int vertex1,
+                                            int vertex2);
+  static void GetShortestPathsBetweenAllVertices(
+      Graph& graph);  // TODO: return value
+  static std::vector<uint32_t> GetLeastSpanningTree(Graph& graph);
+  static TsmResult& SolveTravelingSalesmanProblem(Graph& graph);
 };
 }  // namespace s21
 #endif  // S21_A2_GRAPH_ALGORITHMS_H_
--- src/tests/s21_graph_algorithms_tests.cc
+++ src/tests/s21_graph_algorithms_tests.cc
@@ -3,140 +3,126 @@
 TEST(DepthFirstSearchTestCase, 0) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_undirected_1.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-  EXPECT_EQ(algorithms.DepthFirstSearch(graph, 1), result);
+  EXPECT_EQ(s21::GraphAlgorithms::DepthFirstSearch(graph, 1), result);
 }
 
 TEST(DepthFirstSearchTestCase, 1) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_directed_1.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-  EXPECT_EQ(algorithms.DepthFirstSearch(graph, 1), result);
+  EXPECT_EQ(s21::GraphAlgorithms::DepthFirstSearch(graph, 1), result);
 }
 
 TEST(DepthFirstSearchTestCase, 2) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_directed_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{1,  2, 6,  5,  4,  3,  11, 9,
                                10, 8, 14, 15, 13, 12, 7};
-  EXPECT_EQ(algorithms.DepthFirstSearch(graph, 1), result);
+  EXPECT_EQ(s21::GraphAlgorithms::DepthFirstSearch(graph, 1), result);
 }
 
 TEST(DepthFirstSearchTestCase, 3) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_directed_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{18, 17, 16, 2,  1,  5, 4,  3,  11,
                                9,  10, 8,  14, 15, 6, 13, 12, 7};
-  EXPECT_EQ(algorithms.DepthFirstSearch(graph, 18), result);
+  EXPECT_EQ(s21::GraphAlgorithms::DepthFirstSearch(graph, 18), result);
 }
 
 TEST(DepthFirstSearchTestCase, 4) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_undirected_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{10, 8, 1,  2,  6,  5,  4, 3,
                                11, 9, 14, 15, 13, 12, 7};
-  EXPECT_EQ(algorithms.DepthFirstSearch(graph, 10), result);
+  EXPECT_EQ(s21::GraphAlgorithms::DepthFirstSearch(graph, 10), result);
 }
 
 TEST(DepthFirstSearchTestCase, 5) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_undirected_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{4, 3, 11, 9,  10, 8,  1, 2,
                                6, 5, 14, 15, 7,  12, 13};
-  EXPECT_EQ(algorithms.DepthFirstSearch(graph, 4), result);
+  EXPECT_EQ(s21::GraphAlgorithms::DepthFirstSearch(graph, 4), result);
 }
 
 TEST(DepthFirstSearchTestCase, 6) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_undirected_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{4, 3, 11, 9,  10, 8,  1, 2,
                                6, 5, 14, 15, 7,  12, 13};
-  EXPECT_THROW(algorithms.DepthFirstSearch(graph, 20), std::out_of_range);
+  EXPECT_THROW(s21::GraphAlgorithms::DepthFirstSearch(graph, 20),
+               std::out_of_range);
 }
 
 TEST(BreadthFirstSearchTestCase, 0) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_undirected_1.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-  EXPECT_EQ(algorithms.BreadthFirstSearch(graph, 1), result);
+  EXPECT_EQ(s21::GraphAlgorithms::BreadthFirstSearch(graph, 1), result);
 }
 
 TEST(BreadthFirstSearchTestCase, 1) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_directed_1.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
-  EXPECT_EQ(algorithms.BreadthFirstSearch(graph, 1), result);
+  EXPECT_EQ(s21::GraphAlgorithms::BreadthFirstSearch(graph, 1), result);
 }
 
 TEST(BreadthFirstSearchTestCase, 2) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_directed_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{1,  2,  5,  7,  8,  6, 13, 4,
                                14, 12, 10, 15, 11, 3, 9};
-  EXPECT_EQ(algorithms.BreadthFirstSearch(graph, 1), result);
+  EXPECT_EQ(s21::GraphAlgorithms::BreadthFirstSearch(graph, 1), result);
 }
 
 TEST(BreadthFirstSearchTestCase, 3) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_directed_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{17, 16, 2,  6,  1,  7, 13, 5, 15,
                                8,  4,  12, 11, 14, 9, 10, 3};
-  EXPECT_EQ(algorithms.BreadthFirstSearch(graph, 17), result);
+  EXPECT_EQ(s21::GraphAlgorithms::BreadthFirstSearch(graph, 17), result);
 }
 
 TEST(BreadthFirstSearchTestCase, 4) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_undirected_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{1,  2,  5,  7,  8,  6, 13, 4,
                                14, 12, 10, 15, 11, 3, 9};
-  EXPECT_EQ(algorithms.BreadthFirstSearch(graph, 1), result);
+  EXPECT_EQ(s21::GraphAlgorithms::BreadthFirstSearch(graph, 1), result);
 }
 
 TEST(BreadthFirstSearchTestCase, 5) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_undirected_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{7,  1, 2, 4,  12, 5,  8, 6,
                                13, 3, 9, 14, 10, 15, 11};
-  EXPECT_EQ(algorithms.BreadthFirstSearch(graph, 7), result);
+  EXPECT_EQ(s21::GraphAlgorithms::BreadthFirstSearch(graph, 7), result);
 }
 
 TEST(BreadthFirstSearchTestCase, 6) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_undirected_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{4, 3, 11, 9,  10, 8,  1, 2,
                                6, 5, 14, 15, 7,  12, 13};
-  EXPECT_THROW(algorithms.BreadthFirstSearch(graph, 21), std::out_of_range);
+  EXPECT_THROW(s21::GraphAlgorithms::BreadthFirstSearch(graph, 21),
+               std::out_of_range);
 }
 
 TEST(LeastSpanningTree, 0) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/spanning_tree_1.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{0, 4, 0, 0, 2, 0, 0, 0, 4, 0, 0, 5, 0, 0, 3, 0,
                                0, 0, 0, 0, 0, 4, 1, 0, 0, 5, 0, 0, 0, 0, 0, 0,
                                2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 4,
                                0, 3, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0};
-  EXPECT_EQ(algorithms.GetLeastSpanningTree(graph), result);
+  EXPECT_EQ(s21::GraphAlgorithms::GetLeastSpanningTree(graph), result);
 }
 
 TEST(LeastSpanningTree, 1) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/spanning_tree_2.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{
       0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 3, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -152,13 +138,12 @@ TEST(LeastSpanningTree, 1) {
       0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
       2, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0,
   };
-  EXPECT_EQ(algorithms.GetLeastSpanningTree(graph), result);
+  EXPECT_EQ(s21::GraphAlgorithms::GetLeastSpanningTree(graph), result);
 }
 
 TEST(LeastSpanningTree, 2) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/spanning_tree_3.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result{
       0, 1, 0, 0, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 4, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
@@ -173,28 +158,25 @@ TEST(LeastSpanningTree, 2) {
       0, 6, 0, 0, 0, 0, 0, 0, 0, 7, 0, 0, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 5, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 5, 0};
-  EXPECT_EQ(algorithms.GetLeastSpanningTree(graph), result);
+  EXPECT_EQ(s21::GraphAlgorithms::GetLeastSpanningTree(graph), result);
 }
 
 TEST(LeastSpanningTree, 3) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/graph_directed_2.txt");
-  s21::GraphAlgorithms algorithms;
-  EXPECT_THAT([&]() { algorithms.GetLeastSpanningTree(graph); },
+  EXPECT_THAT([&]() { s21::GraphAlgorithms::GetLeastSpanningTree(graph); },
               testing::ThrowsMessage<std::runtime_error>(
                   testing::HasSubstr("It's not possible to find")));
 }
 
 TEST(LeastSpanningTree, 4) {
   s21::Graph graph;
-  s21::GraphAlgorithms algorithms;
-  EXPECT_TRUE(algorithms.GetLeastSpanningTree(graph).empty());
+  EXPECT_TRUE(s21::GraphAlgorithms::GetLeastSpanningTree(graph).empty());
 }
 
 TEST(LeastSpanningTree, 5) {
   s21::Graph graph;
   graph.LoadGraphFromFile("tests/files/spanning_tree_4.txt");
-  s21::GraphAlgorithms algorithms;
   std::vector<uint32_t> result = {0};
-  EXPECT_EQ(algorithms.GetLeastSpanningTree(graph), result);
+  EXPECT_EQ(s21::GraphAlgorithms::GetLeastSpanningTree(graph), result);
 }
