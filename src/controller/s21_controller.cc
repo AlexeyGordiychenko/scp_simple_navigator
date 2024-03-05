d2fb1a5952d465f3b2a6f5a86b801ef1904cf93c
elidacon <elidacon@student.21-school.ru>
2024-03-05 21:57:32 +0500

    Add Controller class

---
+++ src/controller/s21_controller.cc
@@ -0,0 +1,18 @@
+#include "s21_controller.h"
+
+void s21::Controller::LoadGraphFromFile(const std::string& filename) {
+  graph_.LoadGraphFromFile(filename);
+}
+
+std::vector<uint32_t> s21::Controller::DepthFirstSearch(uint32_t start_vertex) {
+  return s21::GraphAlgorithms::DepthFirstSearch(graph_, start_vertex);
+}
+
+std::vector<uint32_t> s21::Controller::BreadthFirstSearch(
+    uint32_t start_vertex) {
+  return s21::GraphAlgorithms::BreadthFirstSearch(graph_, start_vertex);
+}
+
+std::vector<uint32_t> s21::Controller::GetLeastSpanningTree() {
+  return s21::GraphAlgorithms::GetLeastSpanningTree(graph_);
+}
---
+++ src/controller/s21_controller.h
@@ -0,0 +1,30 @@
+#ifndef S21_A2_CONTROLLER_H_
+#define S21_A2_CONTROLLER_H_
+
+#include "../lib/s21_graph.h"
+#include "../lib/s21_graph_algorithms.h"
+
+namespace s21 {
+
+class Controller {
+ public:
+  Controller() = default;
+  Controller(const Controller&) = delete;
+  Controller& operator=(const Controller&) = delete;
+  Controller(Controller&&) = delete;
+  Controller& operator=(Controller&&) = delete;
+  ~Controller() = default;
+
+  void LoadGraphFromFile(const std::string& filename);
+  std::vector<uint32_t> DepthFirstSearch(uint32_t start_vertex);
+  std::vector<uint32_t> BreadthFirstSearch(uint32_t start_vertex);
+  int GetShortestPathBetweenVertices(int vertex1, int vertex2);
+  void GetShortestPathsBetweenAllVertices();  // TODO: return value
+  std::vector<uint32_t> GetLeastSpanningTree();
+  TsmResult& SolveTravelingSalesmanProblem();
+
+ private:
+  Graph graph_;
+};
+}  // namespace s21
+#endif  // S21_A2_CONTROLLER_H_
