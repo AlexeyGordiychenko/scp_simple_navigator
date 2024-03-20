2d3ef0a8a95386abe6e1ec46e680b3ee74eaf77d
elidacon <elidacon@student.21-school.ru>
2024-03-20 21:21:08 +0500

    Refactor s21_navigator.cc (split into functions)

--- src/s21_navigator.cc
+++ src/s21_navigator.cc
@@ -1,141 +1,144 @@
 #include "controller/s21_controller.h"
 #include "view/s21_console_menu.h"
 
+void Load(s21::ConsoleMenu& menu, s21::Controller& controller) {
+  menu.CallMenuFunction(static_cast<std::function<void(std::string)>>(
+                            [&menu, &controller](std::string filename) {
+                              menu.SetCurrentFile("");
+                              controller.LoadGraphFromFile(filename);
+                              menu.SetCurrentFile(filename);
+                            }),
+                        std::make_tuple("Enter the file path: "));
+}
+
+void Traversal(s21::ConsoleMenu& menu,
+               std::function<std::vector<uint32_t>(uint32_t)> traversal_func,
+               const std::string& traversal_type) {
+  menu.CallMenuFunction(
+      static_cast<std::function<void(uint32_t)>>([&menu, traversal_func,
+                                                  traversal_type](uint32_t a) {
+        menu.PrintVector(traversal_func(a),
+                         traversal_type + "first traversal, starting from " +
+                             std::to_string(a) + ":\n");
+      }),
+      std::make_tuple("Enter the start vertex: "));
+}
+
+void TheShortestPath(s21::ConsoleMenu& menu, s21::Controller& controller) {
+  menu.CallMenuFunction(
+      static_cast<std::function<void(uint32_t, uint32_t)>>(
+          [&menu, &controller](uint32_t a, uint32_t b) {
+            menu.PrintValue(controller.GetShortestPathBetweenVertices(a, b),
+                            "The shortest path is: ");
+          }),
+      std::make_tuple("Enter the first vertex: ", "Enter the second vertex: "));
+}
+
+void TheShortestPathAll(s21::ConsoleMenu& menu, s21::Controller& controller) {
+  menu.CallMenuFunction(
+      static_cast<std::function<void()>>([&menu, &controller]() {
+        menu.PrintMatrix(controller.GetShortestPathsBetweenAllVertices(),
+                         "The shortest path between all pairs of vertices:\n");
+      }),
+      std::make_tuple());
+}
+
+void MinimalSpanningTree(s21::ConsoleMenu& menu, s21::Controller& controller) {
+  menu.CallMenuFunction(
+      static_cast<std::function<void()>>([&menu, &controller]() {
+        menu.PrintMatrix(controller.GetLeastSpanningTree(),
+                         "Minimal spanning tree:\n");
+      }),
+      std::make_tuple());
+}
+
+void TSP(s21::ConsoleMenu& menu, std::function<s21::TsmResult()> tsp_func,
+         const std::string& tsp_type) {
+  menu.CallMenuFunction(
+      static_cast<std::function<void()>>([&menu, tsp_func, tsp_type]() {
+        auto result = tsp_func();
+        menu.PrintVector(
+            result.vertices,
+            "TSM result (" + tsp_type + ") with distance " +
+                std::to_string(static_cast<uint32_t>(result.distance)) + ":\n");
+      }),
+      std::make_tuple());
+}
+
+void TSPCompare(s21::ConsoleMenu& menu, s21::Controller& controller) {
+  menu.CallMenuFunction(
+      static_cast<std::function<void(uint32_t)>>([&menu,
+                                                  &controller](uint32_t n) {
+        menu.MeasureFunctionsTime(
+            n,
+            std::make_pair(
+                std::bind(&s21::Controller::SolveTravelingSalesmanProblem,
+                          &controller),
+                std::string("ant colony algorithm")),
+            std::make_pair(
+                std::bind(
+                    &s21::Controller::SolveTravelingSalesmanProblemAnnealing,
+                    &controller),
+                std::string("annealing algorithm")),
+            std::make_pair(
+                std::bind(&s21::Controller::
+                              SolveTravelingSalesmanProblemNearestNeighbor,
+                          &controller),
+                std::string("nearest neighbor algorithm")));
+      }),
+      std::make_tuple("Enter the number of executions: "));
+}
+
 int main() {
   s21::Controller controller;
   bool running = true;
   s21::ConsoleMenu menu(
       "Choose an option:\n", "Invalid input\n", "> ",
-      {{"Load graph from a file",
-        [&menu, &controller] {
-          menu.CallMenuFunction(static_cast<std::function<void(std::string)>>(
-                                    [&menu, &controller](std::string filename) {
-                                      menu.SetCurrentFile("");
-                                      controller.LoadGraphFromFile(filename);
-                                      menu.SetCurrentFile(filename);
-                                    }),
-                                std::make_tuple("Enter the file path: "));
-        }},
-       {"Bread first traversal",
-        [&menu, &controller] {
-          menu.CallMenuFunction(
-              static_cast<std::function<void(uint32_t)>>(
-                  [&menu, &controller](uint32_t a) {
-                    menu.PrintVector(controller.BreadthFirstSearch(a),
-                                     "Bread first traversal, starting from " +
-                                         std::to_string(a) + ":\n");
-                  }),
-              std::make_tuple("Enter the start vertex: "));
+      {{"Load graph from a file", [&]() { Load(menu, controller); }},
+       {"Bread first traversal 1",
+        [&]() {
+          Traversal(menu,
+                    std::bind(&s21::Controller::BreadthFirstSearch, &controller,
+                              std::placeholders::_1),
+                    "Bread ");
         }},
        {"Depth first traversal",
-        [&menu, &controller] {
-          menu.CallMenuFunction(
-              static_cast<std::function<void(uint32_t)>>(
-                  [&menu, &controller](uint32_t a) {
-                    menu.PrintVector(controller.DepthFirstSearch(a),
-                                     "Depth first traversal, starting from " +
-                                         std::to_string(a) + ":\n");
-                  }),
-              std::make_tuple("Enter the start vertex: "));
+        [&]() {
+          Traversal(menu,
+                    std::bind(&s21::Controller::DepthFirstSearch, &controller,
+                              std::placeholders::_1),
+                    "Depth ");
         }},
        {"The shortest path between any two vertices",
-        [&menu, &controller] {
-          menu.CallMenuFunction(
-              static_cast<std::function<void(uint32_t, uint32_t)>>(
-                  [&menu, &controller](uint32_t a, uint32_t b) {
-                    menu.PrintValue(
-                        controller.GetShortestPathBetweenVertices(a, b),
-                        "The shortest path is: ");
-                  }),
-              std::make_tuple("Enter the first vertex: ",
-                              "Enter the second vertex: "));
-        }},
+        [&]() { TheShortestPath(menu, controller); }},
        {"The shortest paths between all pairs of vertices",
-        [&menu, &controller] {
-          menu.CallMenuFunction(
-              static_cast<std::function<void()>>([&menu, &controller]() {
-                menu.PrintMatrix(
-                    controller.GetShortestPathsBetweenAllVertices(),
-                    "The shortest path between all pairs of vertices:\n");
-              }),
-              std::make_tuple());
-        }},
+        [&]() { TheShortestPathAll(menu, controller); }},
        {"Minimal spanning tree",
-        [&menu, &controller] {
-          menu.CallMenuFunction(
-              static_cast<std::function<void()>>([&menu, &controller]() {
-                menu.PrintMatrix(controller.GetLeastSpanningTree(),
-                                 "Minimal spanning tree:\n");
-              }),
-              std::make_tuple());
-        }},
+        [&]() { MinimalSpanningTree(menu, controller); }},
        {"The salesman problem (ant colony)",
-        [&menu, &controller] {
-          menu.CallMenuFunction(
-              static_cast<std::function<void()>>([&menu, &controller]() {
-                auto result = controller.SolveTravelingSalesmanProblem();
-                menu.PrintVector(
-                    result.vertices,
-                    "TSM result (ant colony) with distance " +
-                        std::to_string(static_cast<uint32_t>(result.distance)) +
-                        ":\n");
-              }),
-              std::make_tuple());
+        [&]() {
+          TSP(menu,
+              std::bind(&s21::Controller::SolveTravelingSalesmanProblem,
+                        &controller),
+              "ant colony");
         }},
        {"The salesman problem (annealing)",
-        [&menu, &controller] {
-          menu.CallMenuFunction(
-              static_cast<std::function<void()>>([&menu, &controller]() {
-                auto result =
-                    controller.SolveTravelingSalesmanProblemAnnealing();
-                menu.PrintVector(
-                    result.vertices,
-                    "TSM result (annealing) with distance " +
-                        std::to_string(static_cast<uint32_t>(result.distance)) +
-                        ":\n");
-              }),
-              std::make_tuple());
+        [&]() {
+          TSP(menu,
+              std::bind(
+                  &s21::Controller::SolveTravelingSalesmanProblemAnnealing,
+                  &controller),
+              "annealing");
         }},
        {"The salesman problem (nearest neighbor)",
-        [&menu, &controller] {
-          menu.CallMenuFunction(
-              static_cast<std::function<void()>>([&menu, &controller]() {
-                auto result =
-                    controller.SolveTravelingSalesmanProblemNearestNeighbor();
-                menu.PrintVector(
-                    result.vertices,
-                    "TSM result (nearest neighbor) with distance " +
-                        std::to_string(static_cast<uint32_t>(result.distance)) +
-                        ":\n");
-              }),
-              std::make_tuple());
+        [&]() {
+          TSP(menu,
+              std::bind(&s21::Controller::
+                            SolveTravelingSalesmanProblemNearestNeighbor,
+                        &controller),
+              "nearest neighbor");
         }},
-       {"Compare TSP algorithms",
-        [&menu, &controller] {
-          menu.CallMenuFunction(
-              static_cast<std::function<void(uint32_t)>>([&menu, &controller](
-                                                             uint32_t n) {
-                menu.MeasureFunctionsTime(
-                    n,
-                    std::make_pair(
-                        std::bind(
-                            &s21::Controller::SolveTravelingSalesmanProblem,
-                            &controller),
-                        std::string("ant colony algorithm")),
-                    std::make_pair(
-                        std::bind(&s21::Controller::
-                                      SolveTravelingSalesmanProblemAnnealing,
-                                  &controller),
-                        std::string("annealing algorithm")),
-                    std::make_pair(
-                        std::bind(
-                            &s21::Controller::
-                                SolveTravelingSalesmanProblemNearestNeighbor,
-                            &controller),
-                        std::string("nearest neighbor algorithm")));
-              }),
-              std::make_tuple("Enter the number of executions: "));
-        }},
+       {"Compare TSP algorithms", [&]() { TSPCompare(menu, controller); }},
        {"Exit", [&running] { running = false; }}});
   while (running) {
     menu();
